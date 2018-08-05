476863863100d0d3327b637c1661b48f75ddbd7a
Zha Wei <zhawei@aidatech.io>
2018-08-05 10:29:21 +0800

    add model function

--- demo/model.py
+++ demo/model.py
@@ -1,12 +1,27 @@
 """
 Mockup model
 """
+import joblib
+import jieba
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Model(object):
 	def __init__(self):
-		pass
+		
+		self.model = joblib.load('demo_model')
+		self.tfidf_vectorizer = joblib.load('demo_tfidf_vectorizer')
+		#jieba.load_userdict(path + '../dict.txt')
+		#self.tokenizer = (lambda string: " ".join([token for token in jieba.cut(string, cut_all=False)]))
+		
 	def predict(self, fact:str):
-		return {'accusation': 'introducing bugs in code', 'term': '100 years'}
+		print("fact: "+fact)
+		fact_token = " ".join([token for token in jieba.cut(fact, cut_all=False)])
+		print("fact_token: "+fact_token)
+		fact_tfidf = self.tfidf_vectorizer.transform(fact_token)
+		print("fact_tfidf:" +fact_tfidf)
+		accu = self.model.predict(fact_tfidf)
+
+		return {'accusation': accu, 'term': '100 years'}
 
 
 model = Model()
